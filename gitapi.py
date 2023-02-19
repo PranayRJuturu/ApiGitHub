984cbe5a77319df6fc19e508530e9d70dd1be0a9
PranayRJuturu <115672420+PranayRJuturu@users.noreply.github.com>
2023-02-19 17:47:16 -0500

    Initial commit

---
+++ gitapi.py
@@ -0,0 +1,29 @@
+import requests
+import json
+import sys
+
+try:
+    userId = input("Enter the GitHub UserId: ")
+    # if len(userId)>39:
+    #     print("User ID cannot be longer than 39 characters!")
+
+    repositoryResponse = requests.get('https://api.github.com/users/'+userId+'/repos')
+    jsonReposiotryResponse = json.loads(repositoryResponse.text)
+  
+    if(repositoryResponse.status_code != 200):
+        raise FileNotFoundError
+        sys.exit()  
+except FileNotFoundError:
+    print("No user found!")
+
+try:
+    for repository in jsonReposiotryResponse:
+
+        commitsResponse = requests.get('https://api.github.com/repos/'+userId+'/'+repository['name']+'/commits')
+        if(commitsResponse.status_code != 200):
+            raise FileNotFoundError
+            sys.exit()
+        jsonCommitsResponse = json.loads(commitsResponse.text)
+        print("Repo:"+repository['name']+" Number of commits:",len(jsonCommitsResponse))
+except FileNotFoundError:
+    print("No commits found!")    
